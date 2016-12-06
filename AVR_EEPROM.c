3df4233ca93b0a713fb882648f9d3dea526ffd60
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2016-12-06 12:15:12 +0530

    added skeleton for basic functions

--- AVR_EEPROM.c
+++ AVR_EEPROM.c
@@ -6,6 +6,47 @@
 // ANKIT BHATNAGAR
 // ANKIT.BHATNAGARINDIA@GMAIL.COM
 //
+// REFERENCE :
+// http://www.avrfreaks.net/forum/tut-c-using-eeprom-memory-avr-gcc?page=all
+//
 //////////////////////////////////////////////
 
+
 #include "AVR_EEPROM.h"
+
+
+uint8_t AVR_EEPROM_read_byte(uint8_t* addr)
+{
+	//READ BYTE FROM THE SPECIFIED EEPROM ADDRESS
+
+}
+
+void AVR_EEPROM_write_byte(uint8_t* addr, uint8_t val)
+{
+	//WRITE BYTE TO THE SPECIFIED EEPROM ADDRESS
+
+}
+
+uint16_t AVR_EEPROM_read_word(uint16_t* addr)
+{
+	//READ WORD(16 BITS) FROM THE SPECIFIED EEPROM ADDRESS
+}
+
+void AVR_EEPROM_write_word(uint16_t* addr, uint16_t val)
+{
+	//WRITE WORD(16 BITS) TO THE SPECIFIED EEPROM ADDRESS
+
+}
+
+void AVR_EEPROM_read_block(void* pointer_eeprom, void* pointer_ram, size_t n)
+{
+	//READ BLOCK OF SPECIFIED SIZE FROM THE SPECIFIED EEPROM ADDRESS AND PLACE
+	//IN THE SPECIFIED RAM BLOCK
+}
+
+void AVR_EEPROM_write_block(void* pointer_ram, void* pointer_eeprom, size_t n)
+{
+	//WRITE THE SPECIFIED RAM BLOCK OF SPECIFIED SIZE IN THE SPECIFIED EEPROM\
+	//ADDRESS
+
+}
--- AVR_EEPROM.h
+++ AVR_EEPROM.h
@@ -6,9 +6,24 @@
 // ANKIT BHATNAGAR
 // ANKIT.BHATNAGARINDIA@GMAIL.COM
 //
+// REFERENCE :
+// http://www.avrfreaks.net/forum/tut-c-using-eeprom-memory-avr-gcc?page=all
+//
 //////////////////////////////////////////////
 
 #ifndef _AVR_EEPROM_H_
 #define _AVR_EEPROM_H_
+
+#include <avr/eeprom.h>
+#include <stdio.h>
+
+uint8_t AVR_EEPROM_read_byte(uint8_t* addr);
+void AVR_EEPROM_write_byte(uint8_t* addr, uint8_t val);
+
+uint16_t AVR_EEPROM_read_word(uint16_t* addr);
+void AVR_EEPROM_write_word(uint16_t* addr, uint16_t val);
+
+void AVR_EEPROM_read_block(void* pointer_eeprom, void* pointer_ram, size_t n);
+void AVR_EEPROM_write_block(void* pointer_ram, void* pointer_eeprom, size_t n);
 
 #endif
